002c8eaf14d0db5ac1517d2cac1ca605c8556a0a
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-09-12 14:14:18 +0200

    Fixing the build

--- csrc/addon.cc
+++ csrc/addon.cc
@@ -42,37 +42,24 @@ void Convert(const FunctionCallbackInfo<Value>& args) {
     reinterpret_cast<const uint8_t*>(input_data), input_length);
   size_t actual_output_length = max_output_length;
 
-  char* output_data = static_cast<char*>(malloc(max_output_length));
-  if (output_data == nullptr) {
-    free(output_data);
-    args.GetReturnValue().Set(Local<Object>());
-    return;
-  }
+  char* output_data[max_output_length];
 
   // Create the Woff2 font
   if (!woff2::ConvertTTFToWOFF2(
     reinterpret_cast<const uint8_t*>(input_data), input_length,
     reinterpret_cast<uint8_t*>(output_data), &actual_output_length
   )) {
-    free(output_data);
     isolate->ThrowException(Exception::TypeError(
         String::NewFromUtf8(isolate, "Could not convert the given font.")));
     return;
   }
 
-  // Reallocate the buffer data if needed
-  if (actual_output_length < max_output_length) {
-    output_data = static_cast<char*>(realloc(output_data, actual_output_length));
-    if (output_data == nullptr) {
-      free(output_data);
-      args.GetReturnValue().Set(Local<Object>());
-      return;
-    }
-  }
-
   Local<Object> outputBuffer;
-  if (!node::Buffer::New(isolate, output_data, actual_output_length).ToLocal(&outputBuffer)) {
-    free(output_data);
+  if (!node::Buffer::Copy(
+    isolate,
+    reinterpret_cast<const char*>(output_data),
+    actual_output_length
+  ).ToLocal(&outputBuffer)) {
     args.GetReturnValue().Set(Local<Object>());
     return;
   }
