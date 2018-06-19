7aadbe3b9024a59791cec28f5af35f6afc4eb567
L2jLiga <L2jLiga@gmail.com>
2018-06-19 18:49:01 +0700

    Removed: unused import from ttf2woff main file

--- bin/ttf2woff2.js
+++ bin/ttf2woff2.js
@@ -1,11 +1,10 @@
 #! /usr/bin/env node
 
-var fs = require('fs');
 var BufferStreams = require('bufferstreams');
 var ttf2woff2 = require('../src');
 
 process.stdin.pipe(new BufferStreams(function(err, buf, cb) {
-  if(err) {
+  if (err) {
     throw err;
   }
   cb(null, ttf2woff2(buf));
