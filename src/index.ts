ff540555e6a55fcd4e7c69a8702167a1f9abfe1e
Nicolas Froidure <nicolas.froidure@gmail.com>
2024-07-20 22:53:41 +0200

    feat(core): better choice between builds
    also allow to debug the version that is in use with the `DEBUG=ttf2woff2` env var.
    
    fix #22
    fix #46

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
-let ttf2woff2: (input: Buffer) => Buffer;
+import { YError, printStackTrace } from 'yerror';
+import debug from 'debug';
+import { env } from 'node:process';
 
-try {
-  ttf2woff2 = (await import('bindings'))('addon.node').convert;
-} catch (err) {
-  ttf2woff2 = (await import('../jssrc/index.js')).default;
+const doDebug = debug('ttf2woff2');
+let ttf2woff2: undefined | ((input: Buffer) => Buffer) = undefined;
+
+if (
+  !env.TTF2WOFF2_VERSION ||
+  env.TTF2WOFF2_VERSION?.toLowerCase() === 'native'
+) {
+  try {
+    ttf2woff2 = (await import('bindings'))('addon.node').convert;
+    doDebug('✅ Using native version.');
+  } catch (err) {
+    doDebug(
+      '❌ Could not load the native version.',
+      printStackTrace(err as Error),
+    );
+  }
 }
 
-export default ttf2woff2;
+if (!env.TTF2WOFF2_VERSION || env.TTF2WOFF2_VERSION?.toLowerCase() === 'wasm') {
+  if (!ttf2woff2) {
+    try {
+      ttf2woff2 = (await import('../jssrc/index.js')).default;
+      doDebug('✅ Using WASM version.');
+    } catch (err) {
+      doDebug(
+        '❌ Could not load the WASM version.',
+        printStackTrace(err as Error),
+      );
+    }
+  }
+}
+
+if (!ttf2woff2) {
+  throw new YError('E_UNABLE_TO_LOAD_TTF2WOFF2', env.TTF2WOFF2_VERSION);
+}
+
+export default ttf2woff2 as NonNullable<typeof ttf2woff2>;
