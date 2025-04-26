7535aa1288f9cb24be3e7026926b2b5973cbde20
Gilles Piou <gilles.piou@gmail.com>
2025-04-26 17:13:23 +0800

    fix(core): fix TS errors

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,27 @@ import bindings from 'bindings'
 import ttf2woff2Loader from '../jssrc/index.js';
 
 const doDebug = debug('ttf2woff2');
-let ttf2woff2: undefined;
+const ttf2woff2 = getExecutable();
 
-if (!env.TTF2WOFF2_VERSION || env.TTF2WOFF2_VERSION?.toLowerCase() === 'native') {
-  try {
-    ttf2woff2 = bindings.default('addon.node').convert;
-    doDebug('✅ Using native version.');
-  } catch (err) {
-    doDebug('❌ Could not load the native version.', printStackTrace(err as Error));
-  }
-}
-
-if (!env.TTF2WOFF2_VERSION || env.TTF2WOFF2_VERSION?.toLowerCase() === 'wasm') {
-  if (!ttf2woff2) {
+function getExecutable() {
+  if (!env.TTF2WOFF2_VERSION || env.TTF2WOFF2_VERSION?.toLowerCase() === 'native') {
     try {
-      ttf2woff2 = ttf2woff2Loader;
+      doDebug('✅ Using native version.');
+      return bindings.default('addon.node').convert;
+    } catch (err) {
+      doDebug('❌ Could not load the native version.', printStackTrace(err as Error));
+    }
+  }
+
+  if (!env.TTF2WOFF2_VERSION || env.TTF2WOFF2_VERSION?.toLowerCase() === 'wasm') {
+    try {
       doDebug('✅ Using WASM version.');
+      return ttf2woff2Loader;
     } catch (err) {
       doDebug('❌ Could not load the WASM version.', printStackTrace(err as Error));
     }
   }
-}
 
-if (!ttf2woff2) {
   throw new YError('E_UNABLE_TO_LOAD_TTF2WOFF2', env.TTF2WOFF2_VERSION);
 }
 
